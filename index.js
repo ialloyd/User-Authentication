a15c557357de98364a7f8f91ea79a06c3577b887
ialloyd <iamalloydfds@gmail.com>
2023-09-23 18:30:11 +0530

    final draft

---
+++ index.js
@@ -0,0 +1,48 @@
+const inputElements = document.querySelectorAll('input')
+const message = document.getElementById('message')
+
+if (localStorage.getItem('user')) {
+
+    window.location.href = 'profile.html';
+
+}
+function generateAccessToken() {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 16; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+document.getElementById('signupForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (inputElements[0].value === '' || inputElements[1].value === '' || inputElements[2].value === '' || inputElements[3].value === '') {
+
+        message.textContent = "All fields are mandatory!";
+        return;
+
+    }
+    else if (inputElements[2].value !== inputElements[3].value) {
+
+        message.textContent = "Passwords didn't match!";
+        return;
+
+    }
+
+    const userDetails = {
+        name: inputElements[0].value,
+        email: inputElements[1].value,
+        password: inputElements[2].value,
+        accessToken: generateAccessToken()
+    };
+    inputElements[0].value = '';
+    inputElements[1].value = '';
+    inputElements[2].value = '';
+
+    localStorage.setItem('user', JSON.stringify(userDetails));
+    message.textContent = 'Signup Successful!'
+    setTimeout(() => { window.location.href = 'profile.html'; }, 2000);
+
+});
---
+++ profile.js
@@ -0,0 +1,36 @@
+
+if (!localStorage.getItem('user')) {
+
+    window.location.href = 'index.html';
+
+} else {
+
+
+    const userDetails = JSON.parse(localStorage.getItem('user'));
+    const container = document.getElementsByClassName('container')[0];
+
+    const profile = document.createElement('div');
+    profile.classList.add('profile');
+
+    profile.innerHTML =
+        `<p>Profile</p>
+    <span class="material-symbols-outlined">
+        person
+    </span>
+    <p>Full Name : ${userDetails.name}</p>
+    <p>Email : ${userDetails.email}</p>
+    <p>Token : ${userDetails.accessToken}</p>
+    <p>Password : ${userDetails.password}</p>
+    <button>LOGOUT</button>`
+
+    container.appendChild(profile);
+
+}
+
+const logoutBtn = document.getElementsByTagName('button')[0];
+
+logoutBtn.addEventListener('click', function () {
+
+    localStorage.removeItem('user');
+    window.location.href = 'index.html';
+});
